f289b061d05275c8aa43f8e520bd77e8776b79fa
kyu-suke <ykaneoka7@gmail.com>
2019-03-01 16:19:55 +0900

    init

---
+++ src/components/CloseModal.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button @click="showModal">show</button>
+    {{show}}
+    <modal v-if="show" :show="show" :closeModal="closeModal"/>
+  </div>
+</template>
+
+<script>
+import Modal from './Modal.vue'
+
+export default {
+  name: 'CloseModal',
+  data() {
+    return {
+      show: false
+    }
+  },
+  methods: {
+    showModal() {
+      this.show = true
+    },
+    closeModal() {
+      this.show = false
+    }
+  },
+  components: {
+    Modal
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="overlay"v-if="show" @click="closeModal">
+    <div class="content">
+      {{show}}
+      <button @click="closeModal">close</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+  props: {
+    show: Boolean,
+    closeModal: Function
+  },
+  created() {
+    document.addEventListener('keyup', this.close)
+  },
+  destroyed() {
+    document.removeEventListener('keyup', this.close)
+  },
+  methods: {
+    close(e) {
+      if (e.code === 'Escape') {
+        this.closeModal()
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  background: rgba(0,0,0, .5);
+  z-index:10;
+  height:100%;
+  width: 100%
+}
+.content {
+  background: white;
+  width: 100px;
+  height: 100px;
+  position: relative;
+  left: 43%;
+  top: 43%;
+}
+</style>
